cb6d20443df99ca8db68171cad7443177a02d0d0
vortex610 <vortexuelpm@gmail.com>
2016-06-21 11:16:48 +0300

    Update run_shaker.sh

--- run_shaker.sh
+++ run_shaker.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #set -x
-#This script should be run from the Master node in order to install and launch Shaker agents
+#This script should be run from the Master node in order to install and launch Shaker
 export DATE=`date +%Y-%m-%d_%H:%M`
 
 ####################### Catching scenarios ##############################################################################################
-
 curl -s 'https://raw.githubusercontent.com/vortex610/shaker/master/nodes_ALL.yaml' > nodes.yaml
 curl -s 'https://raw.githubusercontent.com/vortex610/shaker/master/VMs_ALL.yaml' > VMs.yaml
 
-#Define global variables
 #Define SSH template:
 export SSH_OPTS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'
 #USER_NAME=root
@@ -33,16 +31,16 @@ ssh ${SSH_OPTS} $CONTROLLER_ADMIN_IP "cat > ${REMOTE_SCRIPT}" <<EOF
 source /root/openrc
 SERVER_ENDPOINT=$CONTROLLER_PUBLIC_IP
 #echo "SERVER_ENDPOINT: \$SERVER_ENDPOINT:\$SERVER_PORT"
-#printf 'deb http://ua.archive.ubuntu.com/ubuntu/ trusty universe' > /etc/apt/sources.list
-#apt-get update
-#apt-get -y install iperf python-dev libzmq-dev python-pip && pip install pbr pyshaker
-#iptables -I INPUT -s 10.20.0.0/16 -j ACCEPT
-#iptables -I INPUT -s 10.0.0.0/16 -j ACCEPT
-#iptables -I INPUT -s 172.16.0.0/16 -j ACCEPT
-#iptables -I INPUT -s 192.168.0.0/16 -j ACCEPT
-#shaker-image-builder --debug
-#nova flavor-delete shaker-flavor
-#nova flavor-create shaker-flavor auto 4096 40 8
+printf 'deb http://ua.archive.ubuntu.com/ubuntu/ trusty universe' > /etc/apt/sources.list
+apt-get update
+apt-get -y install iperf python-dev libzmq-dev python-pip && pip install pbr pyshaker
+iptables -I INPUT -s 10.20.0.0/16 -j ACCEPT
+iptables -I INPUT -s 10.0.0.0/16 -j ACCEPT
+iptables -I INPUT -s 172.16.0.0/16 -j ACCEPT
+iptables -I INPUT -s 192.168.0.0/16 -j ACCEPT
+shaker-image-builder --debug
+nova flavor-delete shaker-flavor
+nova flavor-create shaker-flavor auto 4096 40 8
 cp /usr/local/lib/python2.7/dist-packages/shaker/engine/aggregators/traffic.py /usr/local/lib/python2.7/dist-packages/shaker/engine/aggregators/traffic.py.orig
 EOF
 #Run script on remote node
@@ -61,13 +59,13 @@ for item in ${COMPUTE_IP_ARRAY[@]};do
 	REMOTE_SCRIPT2=`ssh ${SSH_OPTS} $item "mktemp"`
 	ssh ${SSH_OPTS} $item "cat > ${REMOTE_SCRIPT2}" <<EOF
 #set -x
-#printf 'deb http://ua.archive.ubuntu.com/ubuntu/ trusty universe' > /etc/apt/sources.list
-#apt-get update
-#apt-get -y install iperf python-dev libzmq-dev python-pip && pip install pbr pyshaker
-#iptables -I INPUT -s 10.20.0.0/16 -j ACCEPT
-#iptables -I INPUT -s 10.0.0.0/16 -j ACCEPT
-#iptables -I INPUT -s 172.16.0.0/16 -j ACCEPT
-#iptables -I INPUT -s 192.168.0.0/16 -j ACCEPT
+printf 'deb http://ua.archive.ubuntu.com/ubuntu/ trusty universe' > /etc/apt/sources.list
+apt-get update
+apt-get -y install iperf python-dev libzmq-dev python-pip && pip install pbr pyshaker
+iptables -I INPUT -s 10.20.0.0/16 -j ACCEPT
+iptables -I INPUT -s 10.0.0.0/16 -j ACCEPT
+iptables -I INPUT -s 172.16.0.0/16 -j ACCEPT
+iptables -I INPUT -s 192.168.0.0/16 -j ACCEPT
 EOF
 	ssh ${SSH_OPTS} $item "bash ${REMOTE_SCRIPT2}"
 	agent_id="a-00$cnt"
@@ -102,7 +100,7 @@ done
 
 ############################### Runing scenarios ##############################################################################################
 
-echo "Run scenarios vor VMs"
+echo "Run scenarios for VMs"
 REMOTE_SCRIPT3=`ssh ${SSH_OPTS} $CONTROLLER_ADMIN_IP "mktemp"`
 ssh ${SSH_OPTS} $CONTROLLER_ADMIN_IP "cat > ${REMOTE_SCRIPT3}" <<EOF
 #set -x
@@ -113,7 +111,7 @@ shaker --server-endpoint \$SERVER_ENDPOINT:\$SERVER_PORT --scenario /usr/local/l
 EOF
 ssh ${SSH_OPTS} $CONTROLLER_ADMIN_IP "bash ${REMOTE_SCRIPT3}"
 
-echo "Run scenarios vor Nodes"
+echo "Run scenarios for Nodes"
 REMOTE_SCRIPT4=`ssh ${SSH_OPTS} $CONTROLLER_ADMIN_IP "mktemp"`
 ssh ${SSH_OPTS} $CONTROLLER_ADMIN_IP "cat > ${REMOTE_SCRIPT4}" <<EOF
 #set -x
@@ -126,11 +124,12 @@ EOF
 ssh ${SSH_OPTS} $CONTROLLER_ADMIN_IP "bash ${REMOTE_SCRIPT4}"
 
 ##################### Cleaning after nodes testing ########################################
-
 for proc in ${COMPUTE_IP_ARRAY[@]};do
 	ssh ${SSH_OPTS} $proc "ps -ef | grep iperf | awk '{print \$2}' | xargs kill"
 	ssh ${SSH_OPTS} $proc "ps -ef | grep shaker | awk '{print \$2}' | xargs kill"
 done
+
+########################### Copying reports to Fuel master node ###########################
 export BUILD=`cat /etc/fuel_build_id`
 scp $CONTROLLER_ADMIN_IP:/root/VMs_$DATE.html /root/VMs_build\-$BUILD\-$DATE.html
 scp $CONTROLLER_ADMIN_IP:/root/nodes_$DATE.html /root/nodes_build\-$BUILD\-$DATE.html
