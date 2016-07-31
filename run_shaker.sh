31931df7f2f7028ea1f73df340d705db5e2edfaf
vortex610 <vortexuelpm@gmail.com>
2016-07-31 18:15:53 +0300

    Update run_shaker.sh

--- run_shaker.sh
+++ run_shaker.sh
@@ -143,8 +143,14 @@ done
 export BUILD=`cat /etc/fuel_build_id`
 scp $CONTROLLER_ADMIN_IP:/root/VMs_$DATE.html /root/VMs_build\-$BUILD\-$DATE.html
 scp $CONTROLLER_ADMIN_IP:/root/nodes_$DATE.html /root/nodes_build\-$BUILD\-$DATE.html
-DATA_NODES=$(grep -Po '"median":.*?[^\\]",' /root/nodes_build\-$BUILD\-$DATE.html | sed 's/\,\ \"unit\"\:\ \"Mbit\/s\"\,$//')
-DATA_VM=$(grep -Po '"median":.*?[^\\]",' /root/VMs_build\-$BUILD\-$DATE.html | sed 's/\,\ \"unit\"\:\ \"Mbit\/s\"\,$//')
-echo $DATA_NODES
-echo $DATA_VM
+CUSTOM_THROUGHPUT_NODES=$(grep -Po '"median":.*?[^\\]",' /root/nodes_build\-$BUILD\-$DATE.html | sed 's/\,\ \"unit\"\:\ \"Mbit\/s\"\,$//' | grep -Eo "[0-9]*" | awk '(NR == 1)')
+CUSTOM_THROUGHPUT__VMS=$(grep -Po '"median":.*?[^\\]",' /root/nodes_build\-$BUILD\-$DATE.html | sed 's/\,\ \"unit\"\:\ \"Mbit\/s\"\,$//' | grep -Eo "[0-9]*" | awk '(NR == 1)')
+CUSTOM_STDEV_NODES=$(grep -Po '"median":.*?[^\\]",' /root/VMs_build\-$BUILD\-$DATE.html | sed 's/\,\ \"unit\"\:\ \"Mbit\/s\"\,$//' | grep -Eo "[0-9]*" | awk '(NR == 3)')
+CUSTOM_STDEV_VMS=$(grep -Po '"median":.*?[^\\]",' /root/VMs_build\-$BUILD\-$DATE.html | sed 's/\,\ \"unit\"\:\ \"Mbit\/s\"\,$//' | grep -Eo "[0-9]*" | awk '(NR == 3)')
+curl -H "Content-Type: application/json" -u "sgudz@mirantis.com:Kew4SZEQ" "https://mirantis.testrail.com/index.php?/api/v2/get_cases/3&suite_id=1752" > cases.json
+echo "1. Custom throughput nodes $CUSTOM_THROUGHPUT_NODES"
+echo "2. Custom throughput VMs $CUSTOM_THROUGHPUT_VMs"
+echo "3. Custom stdev nodes $CUSTOM_STDEV_NODES"
+echo "4. Custom stdev VMs $CUSTOM_STDEV_NODES"
+cat cases.json
 echo "Done."
