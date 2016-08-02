b3df04b67d1d0a2d92d73d281858e3101b7d1d51
vortex610 <vortexuelpm@gmail.com>
2016-08-02 12:02:14 +0300

    Update run_shaker.sh

--- run_shaker.sh
+++ run_shaker.sh
@@ -7,7 +7,7 @@ export DATE=`date +%Y-%m-%d_%H:%M`
 if $CREATE_NEW_RUN;then
 	curl -H "Content-Type: application/json" -u "sgudz@mirantis.com:Kew4SZEQ" -d '{"suite_id": '$SUITE_ID',"name": "to_delete4","assignedto_id": 89,"include_all": true}' "https://mirantis.testrail.com/index.php?/api/v2/add_run/3" > run_data.json
 	RUN_ID=$(grep -Po '"id":.*?[^\\]"' run_data.json | grep -Po "[0-9]*")
-
+fi
 ########################## Get tests from RUN ###############################
 curl -H "Content-Type: application/json" -u "sgudz@mirantis.com:Kew4SZEQ" "https://mirantis.testrail.com/index.php?/api/v2/get_tests/$RUN_ID" > tests_$RUN_ID.json
 TESTS_IDS=$(grep -Po '"id":.*?[^\\]"' tests_$RUN_ID.json | grep -Po "[0-9]*")
